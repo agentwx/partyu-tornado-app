ac6d118e2d10fcc3dabc47e7f4205eafa2286844
Helder Martins <helder.garay@corp.terra.com.br>
2014-08-21 18:51:29 -0300

    Organized code

--- diy/app.py
+++ diy/app.py
@@ -1,38 +1,31 @@
 # -*- coding: utf-8 -*-
 import tornado.ioloop
-import tornado.web
+from tornado.web import Application
 from tornado.httpclient import AsyncHTTPClient
-from tornado.gen import coroutine, Return
 
-from keys import FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET
+from handlers import GetVenuesHandler
+from foursquare import FoursquareComm
 
-FOURSQUARE_BASE_URL = 'https://api.foursquare.com/v2/venues/search?client_id={client_id}&client_secret={client_secret}'
 
-class MainHandler(tornado.web.RequestHandler):
-    def initialize(self):
+class PartyuApp(Application):
+    def __init__(self, *args, **kwargs):
+        super(PartyuApp, self).__init__(*args, **kwargs)
+
+        #init MongoDB
+        self.db = None
+
+        #init http client
         self.client = AsyncHTTPClient()
 
-    @coroutine
-    def get(self):
-        ll = self.get_query_argument('ll')
+        #init api comms
+        self.comms = {}
+        self.comms['4sq'] = FoursquareComm(self.client)
+        self.comms['fb'] = None
 
-        venues = yield self.get_fq_venues(ll)
-
-        self.write(venues)
-
-    @coroutine
-    def get_fq_venues(self, ll):
-        url = FOURSQUARE_BASE_URL.format(client_id=FOURSQUARE_CLIENT_ID, client_secret=FOURSQUARE_CLIENT_SECRET)
-        url += '&v=20130815&ll={ll}&categoryId=4d4b7105d754a06376d81259'.format(ll=ll)
-
-        response = yield self.client.fetch(url)
-        raise Return(response.body)
-
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
 
 def main(address):
+    application = PartyuApp([(r"/getvenues", GetVenuesHandler),])
+
     application.listen(8080, address)
     tornado.ioloop.IOLoop.instance().start()
 
---
+++ diy/foursquare.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from tornado.gen import coroutine, Return
+from tornado.httpclient import HTTPError
+import json
+
+from keys import FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET
+
+class FoursquareError(Exception):
+    ''' Raised for any error communicating with the API '''
+    pass
+
+class FoursquareComm(object):
+
+    BASE_URL = 'https://api.foursquare.com/v2/{method}?client_id={client_id}&client_secret={client_secret}'\
+                    .format(client_id=FOURSQUARE_CLIENT_ID, client_secret=FOURSQUARE_CLIENT_SECRET, method='{method}')
+
+    VENUES_SEARCH = 'venues/search'
+
+    def __init__(self, client):
+        self.client = client
+
+    @coroutine
+    def get_venues(self, ll):
+        url = FoursquareComm.BASE_URL.format(method=FoursquareComm.VENUES_SEARCH)
+        url += '&v=20130815&ll={ll}&categoryId=4d4b7105d754a06376d81259'.format(ll=ll)
+
+        try:
+            response = yield self.client.fetch(url)
+
+            if response.code != 200:
+                raise FoursquareException(response.code)
+
+            body = json.loads(response.body)
+            venues = body['response']['venues']
+
+        except HTTPError as e:
+            raise FoursquareError(e.code)
+
+        raise Return(venues)
